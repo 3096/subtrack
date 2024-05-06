9c115fc520f7686ac00251efae58b0a5c18f1126
3096 <7045393+3096@users.noreply.github.com>
2024-05-05 21:04:56 -0700

    initial ipml

--- src/ada.rs
+++ src/ada.rs
@@ -4,17 +4,24 @@ pub struct PathX {
     path_regex: Regex,
 }
 
+static RE_MARKER: &str = r"r\*(.+?)\*";
+static AUTO_GROUPS: [(&str, &str); 2] = [(r"\\\[\d\d\\\]", r"\[(\d\d)\]"), (r"\\\?", r"(\?)")];
+
 impl PathX {
-    pub fn new(path: String) -> PathX {
-        let mut path_re = regex::escape(&path);
-        if path.contains("(") {
-            // path
+    pub fn new(path: &str) -> PathX {
+        let marker_re = Regex::new(RE_MARKER).unwrap();
+        let path_re = if marker_re.is_match(path) {
+            panic!("Not implemented")
         } else {
-            path_re = Regex::new(r"\\\[\d\d\\\]")
-                .unwrap()
-                .replace_all(&path_re, r"\[(\d\d)\]")
-                .to_string();
-        }
+            let mut path_re = regex::escape(path);
+            for (auto_re, re) in AUTO_GROUPS.iter() {
+                path_re = Regex::new(auto_re)
+                    .unwrap()
+                    .replace_all(&path_re, re.to_string())
+                    .to_string();
+            }
+            path_re
+        };
         PathX {
             path_regex: Regex::new(&path_re).unwrap(),
         }
@@ -32,23 +39,28 @@ impl PathX {
         }
     }
 
-    pub fn get_path(&self, path_src: &str, groups: Vec<&str>) -> Option<String> {
+    pub fn get_path(&self, path_src: &str, groups: &Vec<String>) -> Result<String, &str> {
         if Some(groups.len() + 1) != self.path_regex.static_captures_len() {
-            return None;
+            println!(
+                "{} {}",
+                groups.len(),
+                self.path_regex.static_captures_len().unwrap()
+            );
+            return Err("Number of groups does not match");
         }
         let Some(caps) = self.path_regex.captures(&path_src) else {
-            return None;
+            return Err("Path does not match");
         };
         let mut path = String::new();
         let mut last_match_end = 0;
-        for (_, cap) in caps.iter().skip(1).enumerate() {
+        for (i, cap) in caps.iter().skip(1).enumerate() {
             let cur_match = cap.unwrap();
             path.push_str(&path_src[last_match_end..cur_match.start()]);
-            path.push_str(groups[cur_match.as_str().parse::<usize>().unwrap()]);
+            path.push_str(groups[i].as_str());
             last_match_end = cur_match.end();
         }
         path.push_str(&path_src[last_match_end..]);
-        Some(path)
+        Ok(path)
     }
 }
 
@@ -58,7 +70,7 @@ mod tests {
 
     #[test]
     fn test_ada_path() {
-        let path = PathX::new("C:\\[00]\\[01]\\[02]".to_string());
+        let path = PathX::new("C:\\[00]\\[01]\\[02]");
         assert!(path.path_regex.is_match("C:\\[00]\\[01]\\[02]"));
         assert_eq!(
             path.path_regex
@@ -82,9 +94,22 @@ mod tests {
             vec!["00", "01", "02"]
         );
         assert_eq!(
-            path.get_path("C:\\[00]\\[01]\\[02]", vec!["11", "22", "33"])
-                .unwrap(),
+            path.get_path(
+                "C:\\[00]\\[01]\\[02]",
+                &vec!["11".to_string(), "22".to_string(), "33".to_string()]
+            )
+            .unwrap(),
             "C:\\[11]\\[22]\\[33]"
+        );
+    }
+
+    #[test]
+    fn test_ada_path_with_parentheses() {
+        let path = PathX::new(r"C:\1\1\(..)3");
+        assert_eq!(
+            path.get_path("C:\\1\\1\\023", &vec!["33".to_string()])
+                .unwrap(),
+            "C:\\1\\1\\333"
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,187 @@
-use clap::{builder::Str, Parser};
-use regex::Regex;
-use std::{fs, path};
-
+use clap::Parser;
+use core::str;
 use serde_json::Value;
+use std::{fs, path::Path};
 
 mod ada;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short = 'i', long, default_value = "output.json")]
+    #[arg(short = 'i', long, default_value = "options.json")]
     options_file: String,
+
+    #[arg(short, long)]
+    test: bool,
+}
+
+struct PathXEntry {
+    dir: String,
+    pathx: ada::PathX,
+    idx: usize,
+}
+
+impl PathXEntry {
+    fn new(path: &str, idx: usize) -> PathXEntry {
+        PathXEntry {
+            dir: Path::new(path)
+                .parent()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string(),
+            pathx: ada::PathX::new(path),
+            idx,
+        }
+    }
+}
+
+struct InputItem {
+    path: String,
+    group_cache: Vec<String>,
+}
+
+struct InputEntry {
+    pathx_entry: PathXEntry,
+    items: Vec<InputItem>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let mut output_path: Option<ada::PathX> = None;
-
     // check if the options file exists
     if !fs::metadata(&args.options_file).is_ok() {
         println!("Error reading options file: {}", args.options_file);
         // print usage
-
         return;
     }
 
     // Read the file
     let op_file_json: Value =
-        serde_json::from_str(&fs::read_to_string(&args.options_file).unwrap()).unwrap();
-    // println!("Processing option file: {}", op_file_path.display());
+        serde_json::from_reader(fs::File::open(&args.options_file).unwrap()).unwrap();
     let mut arg_array = op_file_json
         .as_array()
         .unwrap()
         .iter()
         .map(|v| v.as_str().unwrap());
 
-    while let Some(arg) = arg_array.next() {
-        if arg == "--output" {
-            output_path = Some(ada::PathX::new(arg_array.next().unwrap().to_string()));
+    let mut options: Vec<String> = Vec::new();
+    let mut output_path_option: Option<String> = None;
+    let mut output_pathx_option: Option<PathXEntry> = None;
+    let mut input_entries: Vec<InputEntry> = Vec::new();
+    while let Some(mut arg) = arg_array.next() {
+        match arg {
+            "--output" => {
+                options.push(arg.to_string());
+                arg = arg_array.next().unwrap();
+                output_path_option = Some(arg.to_string());
+                output_pathx_option = Some(PathXEntry::new(arg, options.len()));
+            }
+            "(" => {
+                options.push(arg.to_string());
+                arg = arg_array.next().unwrap();
+                input_entries.push(InputEntry {
+                    pathx_entry: PathXEntry::new(arg, options.len()),
+                    items: Vec::new(),
+                });
+            }
+            _ => (),
+        }
+        options.push(arg.to_string());
+    }
+
+    let Some(output_path) = output_path_option else {
+        println!("Error: output path not found in options file");
+        return;
+    };
+    let Some(output_pathx_entry) = output_pathx_option else {
+        println!("Error: output path not found in options file");
+        return;
+    };
+
+    for input_entry in input_entries.iter_mut() {
+        let Ok(dir_items) = fs::read_dir(&input_entry.pathx_entry.dir) else {
+            println!("Error reading directory: {}", input_entry.pathx_entry.dir);
+            return;
+        };
+
+        for dir_item in dir_items {
+            let Ok(dir_item) = dir_item else {
+                println!("Error reading directory entry");
+                return;
+            };
+
+            let Some(item_groups) = input_entry
+                .pathx_entry
+                .pathx
+                .get_groups(dir_item.path().to_str().unwrap())
+            else {
+                continue;
+            };
+
+            input_entry.items.push(InputItem {
+                path: dir_item.path().to_str().unwrap().to_string(),
+                group_cache: item_groups,
+            });
+        }
+    }
+
+    // check all input entries have the same number of items
+    let num_items = input_entries[0].items.len();
+    for input_entry in input_entries.iter().skip(1) {
+        if input_entry.items.len() != num_items {
+            println!("Error: input entries have different number of items");
+            return;
+        }
+    }
+
+    // check all input entries have the same groups
+    let entry_groups_colletion: Vec<&Vec<String>> = input_entries[0]
+        .items
+        .iter()
+        .map(|i| &i.group_cache)
+        .collect();
+    for input_entry in input_entries.iter().skip(1) {
+        if input_entry
+            .items
+            .iter()
+            .map(|i| &i.group_cache)
+            .collect::<Vec<&Vec<String>>>()
+            != entry_groups_colletion
+        {
+            println!("Error: input entries have different groups");
+            return;
+        }
+    }
+
+    for i in 0..num_items {
+        options[output_pathx_entry.idx] = output_pathx_entry
+            .pathx
+            .get_path(&output_path, entry_groups_colletion[i])
+            .unwrap();
+        for input_entry in input_entries.iter() {
+            options[input_entry.pathx_entry.idx] = input_entry
+                .pathx_entry
+                .pathx
+                .get_path(&input_entry.items[i].path, entry_groups_colletion[i])
+                .unwrap();
+        }
+
+        if args.test {
+            println!("options: for group {}", entry_groups_colletion[i].join(","));
+            for opt in options.iter() {
+                println!("{}", opt);
+            }
+            println!("----------------");
+        } else {
+            println!("Merging for group {}", entry_groups_colletion[i].join(","));
+            let cmd = std::process::Command::new("mkvmerge")
+                .args(&options)
+                .output()
+                .expect("failed to execute process");
+            println!("{}", str::from_utf8(&cmd.stdout).unwrap());
+            println!("{}", str::from_utf8(&cmd.stderr).unwrap());
+            println!("----------------")
         }
     }
 }
